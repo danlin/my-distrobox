3d31cc340009efcd4451bda65243f2d0ad94e2f2
Daniel Lindenfelser <daniel@lindenfelser.de>
2024-04-29 17:18:42 +0200

    update installer

--- setup.sh
+++ setup.sh
@@ -6,8 +6,9 @@ git config --global init.defaultBranch main
 git config --global pull.rebase true
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+NO_INTERACTIVE=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 yadm clone https://github.com/danlin/danlin
 
 distrobox-export --app neovide
+
