019c386b3eefac8766f3c07fdfe71e034a8c7b5a
Daniel Lindenfelser <daniel@lindenfelser.de>
2024-04-29 16:12:34 +0200

    add more git settings

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 git config --global user.name "Daniel Lindenfelser"
 git config --global user.email daniel@lindenfelser.de
 git config --global init.defaultBranch main
+git config --global pull.rebase true
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
